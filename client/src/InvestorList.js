0e66fd91c356083c14e4c4eea748a68a365eede6
Femi Dokun <30286598+Femi1992@users.noreply.github.com>
2025-03-01 14:34:55 +0000

    comment added

--- client/src/InvestorList.js
+++ client/src/InvestorList.js
@@ -39,6 +39,7 @@ function InvestorsList() {
                 <td>{investor.investor_type}</td>
                 <td>{investor.date_added}</td>
                 <td>{investor.investor_country}</td>
+                {/* need to add commitment */}
                 <td></td>
               </tr>
             ))}
